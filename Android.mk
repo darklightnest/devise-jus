1c49dc8d2bf27c240f69ddac81166e5bc257e765
Hunterxz <90485970+hunterXcitrus@users.noreply.github.com>
2021-10-10 21:21:38 -0300

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -1,9 +1,15 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
 LOCAL_PATH := $(call my-dir)
 
+ifneq ($(filter lemon pomelo citrus lime juice,$(TARGET_DEVICE)),)
+
 include $(CLEAR_VARS)
-LOCAL_MODULE := init.target.rc
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES := etc/init.target.rc
-LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_ETC)/init/hw
-include $(BUILD_PREBUILT)
+
+include $(call all-makefiles-under,$(LOCAL_PATH))
+
+endif
