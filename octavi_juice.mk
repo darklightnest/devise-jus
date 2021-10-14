d47d6a50bde13d7a6e06974db14bdbf1d98c1261
Hunterxz <90485970+hunterXcitrus@users.noreply.github.com>
2021-10-14 19:20:17 -0300

    Update and rename stag_juice.mk to octavi_juice.mk

--- octavi_juice.mk
+++ octavi_juice.mk
@@ -7,9 +7,8 @@
 # Inherit from juice device
 $(call inherit-product, device/xiaomi/juice/device.mk)
 
-# Inherit some common STAG stuff.
-$(call inherit-product, vendor/stag/main.mk)
-BUILD_TYPE := UNOFFICIAL
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/octavi/config/common_full_phone.mk)
 
 TARGET_GAPPS_ARCH := arm64
 TARGET_BOOT_ANIMATION_RES := 1080
@@ -18,10 +17,10 @@ TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_INCLUDE_LIVE_WALLPAPERS := false
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := stag_juice
+PRODUCT_NAME := octavi_juice
 PRODUCT_DEVICE := juice
 PRODUCT_BRAND := Xiaomi
-PRODUCT_MODEL := Juice
+PRODUCT_MODEL := sm6115
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
